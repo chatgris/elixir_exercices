d844e2a2a77dae9d89a83da7d3708dbebb3a71bb
chatgris <jboyer@af83.com>
2013-07-17 20:10:44 +0200

    Add MyList#sum
    Signed-off-by: chatgris <jboyer@af83.com>

--- lists/mylist.exs
+++ lists/mylist.exs
@@ -14,4 +14,10 @@ defmodule MyList do
   def map([], _func), do: []
 
   def map([ head | tail ], func), do: [func.(head) | map(tail, func)]
+
+  def sum(list), do: _sum(list, 0)
+
+  defp _sum([], total), do: total
+
+  defp _sum([head | tail], total), do: _sum(tail, head + total)
 end
