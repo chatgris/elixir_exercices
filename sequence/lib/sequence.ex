7410c91562af2a55a2f3c72aa1ae67e706f01f95
chatgris <jboyer@af83.com>
2013-07-27 15:51:39 +0200

    Transform Sequence in otp application
    Signed-off-by: chatgris <jboyer@af83.com>

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -1,2 +1,5 @@
 defmodule Sequence do
+  def start(_type, initial_number) do
+    Sequence.Supervisor.start_link(initial_number)
+  end
 end
--- sequence/mix.exs
+++ sequence/mix.exs
@@ -10,7 +10,10 @@ defmodule Sequence.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [
+      mod: { Sequence, 456},
+      registered: [:sequence]
+    ]
   end
 
   # Returns the list of dependencies in the format:
